3f5db01b8dbc2a204199ec943ab1cdf0b32b285e
Keegan Berry <keeganberry@gmail.com>
2015-05-14 23:11:45 -0400

    adding pages and content

--- js/app.js
+++ js/app.js
@@ -3,20 +3,60 @@ app = angular.module('wubsite', ['ngRoute']);
 app.config( function ($routeProvider) {
   $routeProvider
     .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeCtrl'
+    })
+    .when('/about', {
       templateUrl: 'pages/about.html',
       controller: 'aboutCtrl'
     })
-    .when('/projects', {
+    .when('/work', {
       templateUrl: 'pages/projects.html',
       controller: 'projectsCtrl'
     })
+    .when('/thing', {
+      templateUrl: 'pages/thing.html',
+      controller: 'thingCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'pages/contact.html',
+      controller: 'contactCtrl'
+    })
+    // .when('/contact', {
+    //   templateUrl: 'pages/contact.html',
+    //   controller: 'contactCtrl'
+    // })
     .when('/resume', {
       templateUrl: 'pages/resume.html',
       controller: 'resumeCtrl'
+    })
+    .when('/links', {
+      templateUrl: 'pages/cool-links.html',
+      controller: 'linksCtrl'
     })
     .otherwise('/');
 });
 
 app.controller('aboutCtrl', function ($scope, $rootScope) {
   $rootScope.bodyClass = "about";
+});
+
+app.controller('homeCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "home";
+});
+
+app.controller('projectsCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "projects";
+});
+
+app.controller('contactCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "email";
+});
+
+app.controller('thingCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "thing";
+});
+
+app.controller('linksCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "links";
 });
